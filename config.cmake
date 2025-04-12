51e7a883f90b12023622144ee938a06b634c8158
alexlin <me@a1exlin.cn>
2025-04-12 13:50:35 +0800

    测试打包工具

---
+++ config.cmake
@@ -0,0 +1,5 @@
+# config.cmake
+set (TARGET_LIB_ARCH aarch64)
+set (PYBIND11_FINDPYTHON OFF)
+set (PYTHON_EXECUTABLE /home/pi/miniconda3/envs/rknn/bin/python)
+set (GENERATE_C_DEBUG OFF)
---
+++ python/__init__.py
@@ -0,0 +1,1 @@
+from .rkllm import *
